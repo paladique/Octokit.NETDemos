005babd48f0ddc520a01daa17474e37b36c1fcc6
Jasmine <paladique@github.com>
2016-06-08 01:12:32 -0400

    missing

---
+++ SimpleMarkdownCS/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Markdown Maker" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleMarkdownCS._Default"  Async="true" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+            <h2>Write some Markdown!</h2>
+            <p style ="margin: 20px 20px">
+                <asp:TextBox runat="server" TextMode="MultiLine" ID="txtMkd"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button Text="Render it!" runat="server" CssClass ="btn btn-default" OnClick="Unnamed2_Click" />
+            </p>    
+    </div>
+    <div class="row">
+        <asp:Label runat="server" ID ="lblrendered">
+            
+        </asp:Label>
+    </div>
+
+
+</asp:Content>
---
+++ SimpleMarkdownCS/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace SimpleMarkdownCS
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async Task RenderTheMarkdown(GitHubClient client)
+        {
+            
+            NewArbitraryMarkdown mkd = new NewArbitraryMarkdown(txtMkd.Text);
+            string renderedMkd =  await client.Miscellaneous.RenderArbitraryMarkdown(mkd);
+
+            lblrendered.Text = renderedMkd;
+
+        }
+
+        protected async void Unnamed2_Click(object sender, EventArgs e)
+        {
+            var ghClient = new GitHubClient(new ProductHeaderValue("Octokit-Test"));
+            
+            await RenderTheMarkdown(ghClient);
+
+        }
+    }
+}

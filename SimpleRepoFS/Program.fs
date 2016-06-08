ae3252dcc53966d9948b68b36a209ba9631537dc
Jasmine <paladique@github.com>
2016-06-07 23:21:43 -0400

    vb and fs exs

---
+++ SimpleRepoFS/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Threading.Tasks
+open Octokit
+
+
+[<EntryPoint>]
+let main argv = 
+    let client = GitHubClient(new ProductHeaderValue("Octokit-Test"))
+    
+    let task (t: Task) = t.ContinueWith (fun listUserRepos -> ()) |> Async.AwaitTask
+
+    let ListUserRepos =
+        Console.WriteLine("hello");
+    0 // return an integer exit code
+
+    
+
---
+++ SimpleRepoVB/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports Octokit
+
+Module Module1
+
+    Sub Main()
+        Dim ghClient = New GitHubClient(New ProductHeaderValue("Octokit-Test"))
+        Dim Task As Task = New Task(Sub() ListUserRepos(ghClient))
+
+
+        Task.Start()
+        Task.Wait()
+        Console.ReadLine()
+    End Sub
+
+    Private Async Sub ListUserRepos(client As GitHubClient)
+        Dim githubUser As User = Await client.User.Get("paladique")
+        Dim gitubUserRepos As IReadOnlyList(Of Repository) = Await client.Repository.GetAllForUser(githubUser.Login)
+
+        For Each r As Repository In gitubUserRepos
+            Console.WriteLine(r.Name)
+        Next
+    End Sub
+
+End Module
